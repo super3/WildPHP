0a31986b0e909589e2386ca4ab7c983a864ded6c
Shawn Wilkinson <super3boy@gmail.com>
2012-03-14 01:23:35 -0400

    sanitize $page var

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 if( !isset($_GET['page']) ) { $page = 'index'; }
 else { $page = $_GET['page']; }
 
+// Better Sanitize That GET Data
+strtolower ( trim ( $page ) );
+$page = filter_var($page, FILTER_SANITIZE_STRING);
+
 // Get Page
 include_once('view/template/header.php');
 include_once('view/pages/'.$page.'.php'); 
--- view/template/footer.php
+++ view/template/footer.php
@@ -6,8 +6,8 @@
             &copy; <a href="http://super3.org">Super3.org</a> 2012
         </p>
         <p class="right">
-            <a href="index.php?page=contact">Contact</a> / 
-            <a href="index.php?page=about">About</a> 
+            <a href="contact">Contact</a> / 
+            <a href="about">About</a> 
         </p>
     </footer>
 </div> 
