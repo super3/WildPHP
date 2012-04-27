90fbcd370435664bf15d61080febdfdef90109a0
Shawn Wilkinson <super3boy@gmail.com>
2012-04-27 14:37:51 -0400

    added git-ignore

--- index.php
+++ index.php
@@ -7,7 +7,11 @@ else { $page = $_GET['page']; }
 strtolower ( trim ( $page ) );
 $page = filter_var($page, FILTER_SANITIZE_STRING);
 
-// Get Page
+// Check to See if the Page Exsists
+if(!file_exists('view/pages/'.$page.'.php'))
+	$page = '404';
+
+// Show Regular Page
 include_once('view/template/header.php');
 include_once('view/pages/'.$page.'.php'); 
 include_once('view/template/footer.php'); 
